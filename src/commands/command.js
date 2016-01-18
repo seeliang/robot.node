a553b3848f4b2ecc0c1f8f92938a42b7aeda5d94
Seeway <sway.liang@gmail.com>
2016-01-18 23:30:41 +1100

    add move set

--- src/commands/command.js
+++ src/commands/command.js
@@ -1,9 +1,10 @@
 var place = require('./place.js'),
   moveRobot = require('./move.js');
+  turn = require('./turn.js');
 module.exports = function (command, location){
   var feedback = 'Place use PLACE, MOVE, LEFT, RIGHT or REPROT',
     locationArray;
-
+    // note: need input range checker
   if(!global.robot.placed) {
     feedback = 'Pleace place robot with PLACE command';
   }
@@ -14,11 +15,15 @@ module.exports = function (command, location){
     return
   }
 
-  if(command === 'move') {
+  if(command === 'MOVE') {
     moveRobot();
     return
   }
 
+  if(command === 'LEFT'  || command === 'RIGHT') {
+    turn(command);
+    return
+  }
 
   if(command === 'REPORT') {
     console.log (global.robot.report.toString());
--- src/commands/move.js
+++ src/commands/move.js
@@ -1,5 +1,22 @@
 module.exports = function(){
-  if(locationArray[2] === 'EAST') {
+  function _normalizer (number,min,max) {
+    number = (number > max) ? max : number;
+    number = (number < min) ? min : number;
+    return number;
+  }
+  if(global.robot.report[2] === 'EAST') {
+    global.robot.report[0]++;
+  }
+  if(global.robot.report[2] === 'WEST') {
+    global.robot.report[0]--;
+  }
+  if(global.robot.report[2] === 'NORTH') {
     global.robot.report[1]++;
   }
+  if(global.robot.report[2] === 'SOUTH') {
+    global.robot.report[1]--;
+  }
+  global.robot.report[0] = _normalizer(global.robot.report[0], global.robot.range[0], global.robot.range[2] );
+
+  global.robot.report[1] = _normalizer(global.robot.report[1], global.robot.range[1], global.robot.range[3]);
 };
