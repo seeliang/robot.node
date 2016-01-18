9b7fc555bd548e4c24e83eeef0e16eb11983d25c
Seeway <sway.liang@gmail.com>
2016-01-18 23:30:16 +1100

    update init make it more readable

--- src/init.js
+++ src/init.js
@@ -1,10 +1,10 @@
 module.exports = function (){
   if(!global.robot) {
     global.robot = {
-      report: [0,0,'NORTH'],
-      placed: false,
-      facingList: [ 'EAST', 'SOUTH', 'WEST', 'NORTH'],
-      range: [0,0,5,5]
+      report: [1,1,'NORTH'], // [0] towards EAST [1] towards NORTH
+      placed: true,
+      directions: [ 'EAST', 'SOUTH', 'WEST', 'NORTH'],
+      range: [0,0,4,4]
     }
   }
 };
